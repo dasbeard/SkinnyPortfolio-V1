18d62e078c09d908bf5287f3b581cae969f99172
Bryan Jones <thebeard@me.com>
2019-03-19 18:04:09 -0700

    Clean Slate for new Banner Idea

--- src/app/banner/banner.component.ts
+++ src/app/banner/banner.component.ts
@@ -1,7 +1,4 @@
 import { Component, OnInit } from "@angular/core";
-import { BreakpointObserver, Breakpoints } from "@angular/cdk/layout";
-import { Observable } from "rxjs";
-import { map } from "rxjs/operators";
 
 @Component({
   selector: "app-banner",
@@ -9,11 +6,7 @@ import { map } from "rxjs/operators";
   styleUrls: ["./banner.component.css"]
 })
 export class BannerComponent implements OnInit {
-  isHandset$: Observable<boolean> = this.breakpointObserver
-    .observe(Breakpoints.Handset)
-    .pipe(map(result => result.matches));
-
-  constructor(private breakpointObserver: BreakpointObserver) {}
+  constructor() {}
 
   ngOnInit() {}
 }
