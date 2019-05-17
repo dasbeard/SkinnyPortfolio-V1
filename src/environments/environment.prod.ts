ac62f0c249ce447f0f54b56386910557b3d4f240
Bryan Jones <thebeard@me.com>
2019-05-17 11:01:39 -0700

    testing\

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,11 +1,4 @@
 export const environment = {
   production: true,
-  firebase: {
-    apiKey: "AIzaSyDIcpoOFr2bUIME79mt0aJtFSlUwkQoSpM",
-    authDomain: "skinnytemp2.firebaseapp.com",
-    databaseURL: "https://skinnytemp2.firebaseio.com",
-    projectId: "skinnytemp2",
-    storageBucket: "skinnytemp2.appspot.com",
-    messagingSenderId: "191245953019"
-  }
+  //Testing
 };
