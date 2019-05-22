6f5c7a282d17bc16794080e8114ea116ab044c1d
Bryan Jones <thebeard@me.com>
2019-05-22 14:59:22 -0700

    added animation to contact page image

--- src/app/animations.ts
+++ src/app/animations.ts
@@ -13,7 +13,23 @@ export let albumAnimationTransition =
         ]))
     ]);
 
-  
+
+    export let slideRight = 
+        trigger('slideRight', [
+
+            transition('void =>*', [
+
+            query(':enter', style({ opacity: 0 }), {optional: true}),
+                animate('.9s ease-in', keyframes([
+                style({opacity: 0, transform: 'translateX(-175px)', offset: 0}),
+                style({opacity: 0.5, transform: 'translateX(5px)', offset: 0.5}),
+                style({opacity: 1, transform: 'translateX(0)', offset: 1})
+                ]))
+        ])
+    ]);
+    
+
+
   /*
   
   trigger('fadeIn', [
@@ -37,21 +53,21 @@ export let albumAnimationTransition =
 
 
 
-//  !! Saftey
-/*    export let albumSlideFadeIn = trigger('fadeIn', [
-
-        transition('void =>*', [
-
-        query(':enter', style({ opacity: 0 }), {optional: true}),
-        query(':enter', stagger('300ms', [
-            animate('1s 1.2s ease-in', keyframes([
-            style({opacity: 0, transform: 'translateY(175px)', offset: 0}),
-            style({opacity: 0.75, transform: 'translateY(-10px)', offset: 0.3}),
-            style({opacity: 1, transform: 'translateY(0)', offset: 1})
-            ]))
-        ]))
-        ])
-    ]);
+ //  !! Saftey
+     /*    export let albumSlideFadeIn = trigger('fadeIn', [
+     
+             transition('void =>*', [
+     
+             query(':enter', style({ opacity: 0 }), {optional: true}),
+             query(':enter', stagger('300ms', [
+                 animate('1s 1.2s ease-in', keyframes([
+                 style({opacity: 0, transform: 'translateY(175px)', offset: 0}),
+                 style({opacity: 0.75, transform: 'translateY(-10px)', offset: 0.3}),
+                 style({opacity: 1, transform: 'translateY(0)', offset: 1})
+                 ]))
+             ]))
+             ])
+         ]);
   
 */
 
--- src/app/contact-page/contact-page.component.ts
+++ src/app/contact-page/contact-page.component.ts
@@ -1,15 +1,28 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { slideRight } from '../animations';
+import { DataService } from '../data.service';
 
 @Component({
   selector: 'app-contact-page',
   templateUrl: './contact-page.component.html',
-  styleUrls: ['./contact-page.component.css']
+  styleUrls: ['./contact-page.component.css'],
+  animations: [
+    slideRight
+  ]
 })
 export class ContactPageComponent implements OnInit {
+  
+  animate:boolean = false;
 
-  constructor() { }
+  constructor(private dataService: DataService) {
+    this.animate = this.dataService.initialContact;
+   }
 
   ngOnInit() {
+  }
+
+  ngOnDestroy() {
+    this.dataService.initialContact = true;
   }
 
 }
